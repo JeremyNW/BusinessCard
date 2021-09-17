339abd3e8908c416b35ff2a3215d97f1f86f4f31
JeremyNW <jeddynwarren@gmail.com>
2021-09-17 00:38:31 -0600

    Initial commit

--- JeremyCard/ContentView.swift
+++ JeremyCard/ContentView.swift
@@ -9,8 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color(red: 0.05, green: 0.62, blue: 0.72, opacity: 1.0)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("Profile Pic")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 250, height: 250)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                
+                
+                Text("Jeremy Warren")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(.white)
+                    
+         
+                Text("IOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(Color.white)
+                    .frame(height: 50)
+                    .overlay(Text("+1 (385)-207-4183").foregroundColor(.black))
+                
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(Color.white)
+                    .frame(height: 50)
+                    .overlay(Text("jeddynwarren@gmail.com").foregroundColor(.black))
+                
+            } .padding(.all)
+        }
     }
 }
 
